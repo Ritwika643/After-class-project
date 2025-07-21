9a4f42d4910d10008586e08ab5f51690117d34e1
Ritwika643 <ritwikakotturu@gmail.com>
2025-07-21 12:54:00 +0530

    multiplications

---
+++ project20/script.js
@@ -0,0 +1,14 @@
+function xyz() {
+  console.log("this is just a t");
+  console.log("this is just a demo");
+}
+xyz()
+xyz()
+function multiply(a,b,c,d){
+    var multiply = a * b * c * d;
+    return multiply
+}
+
+var result = multiply(1,25,5,3);
+
+console.log(result);

82c3fd71f1481981acb7d9cbeffbfdbd6e200059
Ritwika643 <ritwikakotturu@gmail.com>
2025-08-11 21:05:22 +0530

    brief

---
+++ project25/script.js
@@ -0,0 +1,32 @@
+var a = 50
+var b = 'pqrs'
+
+let k
+
+for(k=0; k<10; k++){
+
+console.log("a")
+
+}
+
+let j=0
+
+while(j < 20){
+
+console.log("bcde")
+
+j++
+
+}
+
+function add(a,b,c){
+
+return a + b + c
+
+}
+
+console.log(add(4,9,6))
+
+var add1 = (a,b,c) => a+b+c
+
+console.log(add1(4,9,6))

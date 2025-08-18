4e37af7d69e50c96aa385d180174ba7dbf183e7c
Ritwika643 <ritwikakotturu@gmail.com>
2025-08-18 21:54:27 +0530

    detail on js

---
+++ project28/script.js
@@ -0,0 +1,14 @@
+var a = 'coding'
+var b = 57
+console.log(typeof(a))
+console.log(typeof(b))
+console.log(typeof(String(b)))
+var c = 89
+console.log(typeof(Number(c)))
+
+try {
+    const i = 'hello world';
+    i = 'this is life'
+} catch (error) {
+    console.log('handling your error' )
+}
